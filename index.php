c2129914a1f0316453745007d12f9fb5e092ac64
9BallHub <karolsowinski2305@gmail.com>
2026-09-10 08:16:33 +0200

    something happened
    all loud on the starting to do shit front

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta charset="UTF-8">
+<title>SDKK</title>
+<link rel="stylesheet" href="styl_1.css">
+</head>
+
+
+<body>
+<section id="container">
+ <header id="head">
+ 
+
+</header>
+
+<aside id="side">
+<img id="rem" src="sdkk rem.png"/>
+
+
+</aside>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+</body>
+
+</html>
